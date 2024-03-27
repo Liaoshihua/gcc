dde04fd751a075875d60b0ed19e6d94c200d270b
Liao Shihua <shihua@iscas.ac.cn>
2024-03-27 18:28:20 +0800

    Revert "Fix profile update in scale_profile_for_vect_loop"
    This reverts commit 760a76d020bdca292c42e16c6ebdf229aa4ba9db.

--- gcc/tree-vect-loop.cc
+++ gcc/tree-vect-loop.cc
@@ -10958,48 +10958,31 @@ vect_get_loop_len (loop_vec_info loop_vinfo, gimple_stmt_iterator *gsi,
 static void
 scale_profile_for_vect_loop (class loop *loop, unsigned vf, bool flat)
 {
-  /* For flat profiles do not scale down proportionally by VF and only
-     cap by known iteration count bounds.  */
-  if (flat)
+  edge preheader = loop_preheader_edge (loop);
+  /* Reduce loop iterations by the vectorization factor.  */
+  gcov_type new_est_niter = niter_for_unrolled_loop (loop, vf);
+  profile_count freq_h = loop->header->count, freq_e = preheader->count ();
+
+  if (freq_h.nonzero_p ())
     {
-      if (dump_file && (dump_flags & TDF_DETAILS))
-	fprintf (dump_file,
-		 "Vectorized loop profile seems flat; not scaling iteration "
-		 "count down by the vectorization factor %i\n", vf);
-      scale_loop_profile (loop, profile_probability::always (),
-			  get_likely_max_loop_iterations_int (loop));
-      return;
+      profile_probability p;
+
+      /* Avoid dropping loop body profile counter to 0 because of zero count
+	 in loop's preheader.  */
+      if (!(freq_e == profile_count::zero ()))
+        freq_e = freq_e.force_nonzero ();
+      p = (freq_e * (new_est_niter + 1)).probability_in (freq_h);
+      scale_loop_frequencies (loop, p);
     }
-  /* Loop body executes VF fewer times and exit increases VF times.  */
+
   edge exit_e = single_exit (loop);
-  profile_count entry_count = loop_preheader_edge (loop)->count ();
+  exit_e->probability = profile_probability::always () / (new_est_niter + 1);
 
-  /* If we have unreliable loop profile avoid dropping entry
-     count bellow header count.  This can happen since loops
-     has unrealistically low trip counts.  */
-  while (vf > 1
-	 && loop->header->count > entry_count
-	 && loop->header->count < entry_count * vf)
-    {
-      if (dump_file && (dump_flags & TDF_DETAILS))
-	fprintf (dump_file,
-		 "Vectorization factor %i seems too large for profile "
-		 "prevoiusly believed to be consistent; reducing.\n", vf);
-      vf /= 2;
-    }
-
-  if (entry_count.nonzero_p ())
-    set_edge_probability_and_rescale_others
-	    (exit_e,
-	     entry_count.probability_in (loop->header->count / vf));
-  /* Avoid producing very large exit probability when we do not have
-     sensible profile.  */
-  else if (exit_e->probability < profile_probability::always () / (vf * 2))
-    set_edge_probability_and_rescale_others (exit_e, exit_e->probability * vf);
-  loop->latch->count = single_pred_edge (loop->latch)->count ();
-
-  scale_loop_profile (loop, profile_probability::always () / vf,
-		      get_likely_max_loop_iterations_int (loop));
+  edge exit_l = single_pred_edge (loop->latch);
+  profile_probability prob = exit_l->probability;
+  exit_l->probability = exit_e->probability.invert ();
+  if (prob.initialized_p () && exit_l->probability.initialized_p ())
+    scale_bbs_frequencies (&loop->latch, 1, exit_l->probability / prob);
 }
 
 /* For a vectorized stmt DEF_STMT_INFO adjust all vectorized PHI
@@ -11620,6 +11603,7 @@ vect_transform_loop (loop_vec_info loop_vinfo, gimple *loop_vectorized_call)
 			   niters_vector_mult_vf, !niters_no_overflow);
 
   unsigned int assumed_vf = vect_vf_for_cost (loop_vinfo);
+  scale_profile_for_vect_loop (loop, assumed_vf);
 
   /* True if the final iteration might not handle a full vector's
      worth of scalar iterations.  */
